f1b380cdbe828e7b59a9a0e2442e71530b4c2c07
jinrui01 <85c895168e6645428988b65c986ab5ec@sf-express.com>
2019-11-30 23:48:00 +0800

    customize

--- Source/Tailwind.Traders.Web/ClientApp/src/pages/customize/customize.js
+++ Source/Tailwind.Traders.Web/ClientApp/src/pages/customize/customize.js
@@ -3,21 +3,11 @@ import { connect } from 'react-redux';
 
 class Customize extends Component {
     render() {
-        const { shoppingCart, loading, isPulling } = this.state
         return (
-            <NamespacesConsumer>
-                {t => (
-                    <Fragment>
-                        {loading && isPulling ? <LoadingSpinner /> : <Fragment>
-                            <h2>Customize</h2>
-                            <div className="shopping-card__grid">
-                                This is new page
-                            </div>
-                        </Fragment>}
-                    </Fragment>
-                )}
-            </NamespacesConsumer>
-        );
+            <div>
+              Customize your garden
+            </div>
+          );
     }
 }
 
